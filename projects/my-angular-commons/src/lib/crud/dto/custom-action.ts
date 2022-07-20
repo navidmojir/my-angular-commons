fe615a52e0fdf55cc9fc18a817013b6377a3f665
Navid Mojir <nav.mojir@gmail.com>
2022-07-20 15:05:30 +0430

    ability to show icon for list operations

--- projects/my-angular-commons/src/lib/crud/dto/custom-action.ts
+++ projects/my-angular-commons/src/lib/crud/dto/custom-action.ts
@@ -2,11 +2,13 @@
 export class CustomAction {
     private _title: string;
     private _onClick: any;
+    private _icon: string;
 
 
-	constructor(title: string, onClick: any) {
+    constructor(title: string, onClick: any, icon: string = null) {
 		this._title = title;
 		this._onClick = onClick;
+        this._icon = icon;
 	}
 
 
@@ -25,6 +27,10 @@ export class CustomAction {
      */
 	public get onClick(): any {
 		return this._onClick;
+	}
+
+    public get icon(): string {
+		return this._icon;
 	}
 
     
