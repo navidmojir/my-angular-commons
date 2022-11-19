fc7ef195bcb93b171ef83e36466603a5ffe4df86
Navid Mojir <nav.mojir@gmail.com>
2022-11-19 10:38:15 +0330

    fix jalali-moment error

--- projects/my-angular-commons/src/lib/pipes/jalali.pipe.ts
+++ projects/my-angular-commons/src/lib/pipes/jalali.pipe.ts
@@ -1,5 +1,5 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import moment from 'jalali-moment';
+import jalaliMoment from 'jalali-moment';
 
 // convert epoch to shamsi
 @Pipe({
@@ -10,7 +10,7 @@ export class JalaliPipe implements PipeTransform {
 	transform(value: any, args?: any): any {
 		if(value == null)
 			return '-';
-		let MomentDate = moment(new Date(value));
+		let MomentDate = jalaliMoment(new Date(value));
 		return MomentDate.locale('fa').format('YYYY/MM/DD'); 
 	}
 
@@ -24,7 +24,7 @@ export class JalaliTimePipe implements PipeTransform {
 	transform(value: any, args?: any): any {		
 		if(value == null)
 			return '-';
-		let MomentDate = moment(new Date(value));
+		let MomentDate = jalaliMoment(new Date(value));
 		return MomentDate.locale('fa').format('HH:mm:ss - YYYY/MM/DD'); 
 	}
 
